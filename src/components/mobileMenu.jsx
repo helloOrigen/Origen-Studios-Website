0c0ee5ea42509585d5c001a7d5dac3b8376183a0
OrigenStudios <122059316+helloOrigen@users.noreply.github.com>
2023-01-20 10:49:10 -0300

    menu-btn

--- src/components/mobileMenu.jsx
+++ src/components/mobileMenu.jsx
@@ -1,15 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment,useState} from "react";
 import "../Styles/seed.css";
 import { Link } from "react-router-dom";
 import NavBarBtn from "./navBarBtn";
+import NavBarLogo from "./navBarLogo";
+import "../Styles/home.css";
 
-export default function mobileMenu(props) {
-  const { className } = props;
+ const mobileMenu=()=> {
+  
+  
 
   return (
+
     <Fragment>
-      <div
-        className={` ${className}  d-n m-d-b of-auto bg-linear-b-t `}
+<div className="d-n m-d-b of-auto bg-linear-b-t" Style="position: fixed; left:0px; right:0px; top: 0px; bottom: 0px; z-index: 99998;">
+
+
+  <input type="checkbox" id="menu"/>
+  <ul>
+  <div
+        className={`  d-n m-d-b of-auto bg-linear-b-t `}
         Style="position: fixed; left:0px; right:0px; top: 0px; bottom: 0px; z-index: 99998;"
       >
         <div className="w-100 h-a r-v-b  h-pr-fl-ma">
@@ -18,7 +27,7 @@ export default function mobileMenu(props) {
               className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
               Style="border-bottom: 1px solid; border-color: white;"
             >
-              <NavBarBtn className="w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Nosotros"}></NavBarBtn>
+              <NavBarBtn className="b w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Nosotros"}></NavBarBtn>
             </div>
           </Link>
           <Link to="/team" className="h-e">
@@ -26,33 +35,25 @@ export default function mobileMenu(props) {
               className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
               Style="border-bottom: 1px solid; border-color: white;"
             >
-              <NavBarBtn className="w-100 h-100 h-pr-fl-ma p-10px p-t-30px"  text={"Estructura"}></NavBarBtn>
+              <NavBarBtn className="b w-100 h-100 h-pr-fl-ma p-10px p-t-30px"  text={"Estructura"}></NavBarBtn>
             </div>
           </Link>
           <Link to="/services" className="h-e">
-             
+            {" "}
             <div
               className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
               Style="border-bottom: 1px solid; border-color: white;"
             >
-              <NavBarBtn className="w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Servicios"}></NavBarBtn>
+              <NavBarBtn className="b w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Servicios"}></NavBarBtn>
             </div>
           </Link>
           <Link to="/plans" className="h-e">
-             
+            {" "}
             <div
               className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
               Style="border-bottom: 1px solid; border-color: white;"
             >
-              <NavBarBtn className="w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Planes"}></NavBarBtn>
-            </div>
-          </Link>
-          <Link to="/plans" className="h-e">
-            <div
-              className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
-              Style="border-bottom: 1px solid; border-color: white;"
-            >
-              <NavBarBtn className="w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Metodología"}></NavBarBtn>
+              <NavBarBtn className="b w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Planes"}></NavBarBtn>
             </div>
           </Link>
           <Link to="/plans" className="h-e">
@@ -60,11 +61,25 @@ export default function mobileMenu(props) {
               className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
               Style="border-bottom: 1px solid; border-color: white;"
             >
-              <NavBarBtn className="w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Parámetros"}></NavBarBtn>
+              <NavBarBtn className="b w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Metodología"}></NavBarBtn>
+            </div>
+          </Link>
+          <Link to="/plans" className="h-e">
+            <div
+              className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
+              Style="border-bottom: 1px solid; border-color: white;"
+            >
+              <NavBarBtn className="w-100 h-100 h-pr-fl-ma p-10px p-t-30px b" text={"Parámetros"}></NavBarBtn>
             </div>
           </Link>
         </div>
-      </div>
-    </Fragment>
+    </div>
+    </ul>
+
+</div>
+   </Fragment>
   );
 }
+
+
+export default mobileMenu
