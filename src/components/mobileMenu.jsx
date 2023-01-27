eb06a0fd3451835c9894636a285755c5b5ba940c
Nickolas Machado <hello@origenstudios.com>
2023-01-27 19:48:17 -0300

    fixes 27-01-2023 07:48

--- src/components/mobileMenu.jsx
+++ src/components/mobileMenu.jsx
@@ -12,19 +12,19 @@ import "../Styles/home.css";
   return (
 
     <Fragment>
-<div className="d-n m-d-b of-auto bg-linear-b-t" Style="position: fixed; left:0px; right:0px; top: 0px; bottom: 0px; z-index: 99998;">
+<div className="d-n m-d-b of-auto  " Style="position: fixed; left:0px; right:0px; top: 0px; bottom: 0px; z-index: 99998;">
 
 
   <input type="checkbox" id="menu"/>
   <ul>
   <div
-        className={`  d-n m-d-b of-auto bg-linear-b-t `}
+        className={`  d-n m-d-b of-auto  m-5px bg-linear-b-t`}
         Style="position: fixed; left:0px; right:0px; top: 0px; bottom: 0px; z-index: 99998;"
       >
         <div className="w-100 h-a r-v-b  h-pr-fl-ma">
           <Link to="/team" className="h-e D">
             <div
-              className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
+              className="w-100 h-a h-pr-fl-ma hover-underline-animation  "
               Style="border-bottom: 1px solid; border-color: white;"
             >
               <NavBarBtn className="b w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Nosotros"}></NavBarBtn>
@@ -32,7 +32,7 @@ import "../Styles/home.css";
           </Link>
           <Link to="/team" className="h-e">
             <div
-              className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
+              className="w-100 h-a h-pr-fl-ma hover-underline-animation  "
               Style="border-bottom: 1px solid; border-color: white;"
             >
               <NavBarBtn className="b w-100 h-100 h-pr-fl-ma p-10px p-t-30px"  text={"Estructura"}></NavBarBtn>
@@ -41,7 +41,7 @@ import "../Styles/home.css";
           <Link to="/services" className="h-e">
             {" "}
             <div
-              className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
+              className="w-100 h-a h-pr-fl-ma hover-underline-animation  "
               Style="border-bottom: 1px solid; border-color: white;"
             >
               <NavBarBtn className="b w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Servicios"}></NavBarBtn>
@@ -50,7 +50,7 @@ import "../Styles/home.css";
           <Link to="/plans" className="h-e">
             {" "}
             <div
-              className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
+              className="w-100 h-a h-pr-fl-ma hover-underline-animation  "
               Style="border-bottom: 1px solid; border-color: white;"
             >
               <NavBarBtn className="b w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Planes"}></NavBarBtn>
@@ -58,7 +58,7 @@ import "../Styles/home.css";
           </Link>
           <Link to="/plans" className="h-e">
             <div
-              className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
+              className="w-100 h-a h-pr-fl-ma hover-underline-animation  "
               Style="border-bottom: 1px solid; border-color: white;"
             >
               <NavBarBtn className="b w-100 h-100 h-pr-fl-ma p-10px p-t-30px" text={"Metodología"}></NavBarBtn>
@@ -66,16 +66,16 @@ import "../Styles/home.css";
           </Link>
           <Link to="/plans" className="h-e">
             <div
-              className="w-100 h-a h-pr-fl-ma hover-underline-animation bg-linear-b-t "
+              className="w-100 h-a h-pr-fl-ma hover-underline-animation  "
               Style="border-bottom: 1px solid; border-color: white;"
             >
               <NavBarBtn className="w-100 h-100 h-pr-fl-ma p-10px p-t-30px b" text={"Parámetros"}></NavBarBtn>
             </div>
           </Link>
           <div
-className="w-100 h-a h-pr-fl-ma hover-underline-animation p-b-20px p-t-20px "
+className="w-100 h-a h-pr-fl-ma p-b-20px p-t-20px "
 
-><div className="m-t-20px m-b-30px w-100 h-100px h-pr-fl-ma t-a-c b-s-b-b" Style="border: 1px solid; border-color: white;"><div className="h-pr-fl-ma w-a h-a centered ">Proximamente Banner Promo</div> </div></div>
+><div className="m-t-20px m-b-30px w-100 h-100px h-pr-fl-ma t-a-c b-s-b-b hover-underline-animation " Style="border: 1px solid; border-color: white;"><div className="h-pr-fl-ma w-a h-a centered ">Proximamente Banner Promo</div> </div></div>
         </div>
     </div>
     </ul>
