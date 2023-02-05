1e6889e1c50a62223a41689de11ddcd336c87f73
OrigenStudios <122059316+helloOrigen@users.noreply.github.com>
2023-02-04 22:42:44 -0300

    Delete src directory

--- src/images.js
+++
@@ -1,9 +0,0 @@
-import Wallpaper1 from "./assets/images/wallpaper-1.png";
-import Wallpaper2 from "./assets/images/wallpaper-2.png";
-import Wallpaper3 from "./assets/images/wallpaper-3.png";
-import Wallpaper4 from "./assets/images/wallpaper-4.png";
-import Wallpaper5 from "./assets/images/wallpaper-5.png";
-import Wallpaper6 from "./assets/images/wallpaper-6.png";
-import Wallpaper7 from "./assets/images/wallpaper-7.png";
-import Wallpaper8 from "./assets/images/wallpaper-8.png";
-export default [Wallpaper1,Wallpaper2,Wallpaper3,Wallpaper4,Wallpaper5,Wallpaper6,Wallpaper7,Wallpaper8]
