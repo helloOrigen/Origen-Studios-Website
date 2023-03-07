2e0f93f63b970e970cc2b50a658da99cd2868ca3
Nickolas Machado <hello@origenstudios.com>
2023-03-06 23:06:01 -0300

    Sitemap y correccion botones

--- src/components/btn.jsx
+++ src/components/btn.jsx
@@ -7,8 +7,8 @@ export default function Btn(props) {
   return (
     <>
       <div
-        className={` ${className} h-pr-fl-ma w-a  h-a p-5px h-e c-p  b-s-b-b ns-c-f-1 c-p h-e hover-underline-animation d-i-f f-f-Gilroy `} Style="border: 1px solid; border-color: white;"
-      >     <a href={enlace} className="w-a h-a h-pr-fl-ma d-i-f">
+        className={` ${className} h-pr-fl-ma w-a  ns-bg-e h-e-ghost-e t-03s  h-a p-5px h-e c-p br-r-5px b-s-b-b ns-c-f-1 c-p h-e d-i-f f-f-Gilroy `} 
+      >     <a href={enlace} className="w-a h-a h-pr-fl-ma d-i-f ">
       <div className="w-a h-a h-pr-fl-ma ">{children}</div>
         <div
           className="w-a h-a h-pr-fl-ma t-a-c c-white d-i-f"
